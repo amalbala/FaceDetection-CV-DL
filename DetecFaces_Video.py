cc55fd17dca3220730eaab2e411807ec7d7ab311
Antonio Martinez <amalbala@gmail.com>
2019-05-29 15:49:07 +0200

    Formating

--- DetecFaces_Video.py
+++ DetecFaces_Video.py
@@ -35,14 +35,14 @@ while True:
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
             text = "{:.2f}%".format(confidence * 100)
-            y = startY - 10 if startY -10 > 10 else startY + 10
+            y = startY - 10 if startY - 10 > 10 else startY + 10
             cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
             cv2.putText(frame, text, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) % 0XFF
 
-    if(key == ord("q")):
+    if key == ord("q"):
         break
 
 cv2.destroyAllWindows()
