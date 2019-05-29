8ccd6e56df84a3c78b1a5b08303105ea329ce38b
Antonio Martinez <amalbala@gmail.com>
2019-05-29 15:17:56 +0200

    Cleaning

--- DetecFaces_Video.py
+++ DetecFaces_Video.py
@@ -19,7 +19,7 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 while True:
     frame = vs.read()
-    frame = imutils.resize(frame, width = 400)
+    frame = imutils.resize(frame, width=400)
 
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
